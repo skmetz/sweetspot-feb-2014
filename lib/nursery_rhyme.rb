4b02da8ef279a23a7a36529bf224fee019b102bb
Sandi Metz <skmetz@gmail.com>
2014-02-24 05:43:30 -0800

    pretty simple house

---
+++ lib/nursery_rhyme.rb
@@ -0,0 +1,27 @@
+class NurseryRhyme
+  PLOT = [
+    ['house',   'Jack built'],
+    ['malt',    'lay in'],
+    ['rat',     'ate'],
+    ['cat',     'killed'],
+    ['dog',     'worried'],
+    ['cow with the crumpled horn',    'tossed'],
+    ['maiden all forlorn',            'milked'],
+    ['man all tattered and torn',     'kissed'],
+    ['priest all shaven and shorn',   'married'],
+    ['cock that crowed in the morn',  'woke'],
+    ['farmer sowing his corn',        'kept'],
+    ['horse and the hound and the horn', 'belonged to']
+  ]
+  def line(number)
+    "This is#{inner(number)}.\n"
+  end
+
+  def inner(number)
+    (number - 1).downto(0).collect do |i|
+      " the %s that %s" % [PLOT[i][0], PLOT[i][1]]
+    end.join('')
+  end
+
+end
+
